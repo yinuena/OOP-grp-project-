983edb3141b759549e9675c881231cb319cdef45
rasyidahmhmmdn <126145569+rasyidahmhmmdn@users.noreply.github.com>
2023-03-15 15:51:11 +0800

    Update Name.java

--- src/Name.java
+++ src/Name.java
@@ -12,13 +12,25 @@ public class Name {
     public String getFname() {
         return fname;
     }
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
     public String getMname() {
         return mname;
     }
+
+    public void setMname(String mname) {
+        this.mname = mname;
+    }
+
     public String getLname() {
         return lname;
     }
 
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
 
     @Override
     public String toString() {
