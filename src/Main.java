00121514148e7d54309868cb089f3a7a0be5cb86
yinuena <126445864+yinuena@users.noreply.github.com>
2023-03-28 16:50:29 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //menu
+
+        String username = " ";
+        String email = null;
+        String password = null;
+        int age = 0;
+        String fname = null;
+        String mname = null;
+        String lname = null;
+        int weeknum;
+
         int option = 0;
         while (option != 5) {
-            System.out.println("----- Welcome to HelloMYCa$h----- \n [1] Login \n [2] Check Login Details \n [3] Set Budget \n [4] Key in Expense \n [5]Quit");
+            System.out.println("----- Welcome to HelloMYCa$h----- \n [1] Login \n [2] Check Login Details \n [3] Set Budget & Expense \n [4] Date \n [5]Quit");
             System.out.print("Enter your choice of menu: ");
             option = sc.nextInt();
 
-
-            String username = null;
-            String email = null;
-            String password = null;
-            int age = 0;
-            String fname = null;
-            String mname = null;
-            String lname = null;
-            int weeknum;
 
             User user = new User();
 
@@ -55,28 +56,27 @@ public class Main {
             if (option == 2) {
 
                 User u = new User();
-                u.getUsername(username);
+
+                u.setUsername(username);
                 u.setEmail(email);
                 u.setPassword(password);
                 u.setAge(age);
-                System.out.println("Your Details:");
-                System.out.println("Username : " + u.getUsername());
-                System.out.println("Email :" + u.getEmail());
-                System.out.println("Password :" + u.getPassword());
-                System.out.println("Age: " + u.getAge());
+
+                u.display();
+
 
                 Name n = new Name();
                 n.setFname(fname);
                 n.setMname(mname);
                 n.setLname(lname);
-
-
-                System.out.println("First name: " + n.getFname());
-                System.out.println("Middle name: " + n.getMname());
-                System.out.println("Last name: " + n.getLname());
-
+                
+              
+                n.DisplayName();
 
                 //sc.close();
+            }
+            
+            if(option == 4){  
 
                 Calendar cal = Calendar.getInstance();
                 SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
@@ -92,6 +92,7 @@ public class Main {
                 //get currency from user
                 Budget budget = new Budget();
                 System.out.println("[3] Set Budget");
+                sc.nextLine();
                 System.out.print("Enter the currency you want to proceed with:  ");
                 String currency = sc.nextLine();
                 budget.setCurrency(currency);
@@ -99,6 +100,8 @@ public class Main {
                 //get budget from userr
                 System.out.print("Enter the week of the month you want to set the budget to: ");
                 int index = sc.nextInt();
+
+                //error checking
                 if ((index > 4) || (index <= 0)) {
                     index = 0;
                     System.out.println("invalid input ");
@@ -123,12 +126,13 @@ public class Main {
 
 
                     // prompt the user for input
+                    sc.nextLine();
                     System.out.print("Enter the date of the expense (YYYY-MM-DD): ");
                     String dateString = sc.nextLine();
 
-                    System.out.print("Enter the amount of the expense: ");
-                    double Eamount = sc.nextDouble();
-                    sc.nextLine(); // consume the newline character
+                    //System.out.print("Enter the amount of the expense: ");
+                    //double Eamount = sc.nextDouble();
+                    //sc.nextLine(); // consume the newline character
 
                     System.out.print("Enter the notes of the expense: ");
                     String notes = sc.nextLine();
@@ -140,34 +144,34 @@ public class Main {
                     Expense expense = new Expense(dateString, amount, currency, notes);
 
 
-                    System.out.print("Do you want to add another expense? (y/n): ");
+                    System.out.print("Do you want to add another expense to another date? (y/n): ");
                     String choice = sc.nextLine();
                     if (choice.equals("n")) {
-                        break;
+                      break;
                     }
+                    }
+
+                    double[] expenses = new double[7];
+                    String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+                    // Input expenses for each day of the week
+                    for (int i = 0; i < 7; i++) {
+                        System.out.print("Enter expenses for " + daysOfWeek[i] + ": ");
+                        expenses[i] = sc.nextDouble();
+
+                    }
+
+
+                    // Calculate total weekly expense
+                    double totalExpense = 0;
+                    for (double expense : expenses) {
+                        totalExpense += expense;
+                    }
+
+                    // Print total weekly expense
+                    System.out.printf("Total weekly expense week ", weeknum, " : $%.2f\n", totalExpense);
+                    budget.ChkExceedBudget(totalExpense, weeknum);
                 }
-
-                double[] expenses = new double[7];
-                String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-                // Input expenses for each day of the week
-                for (int i = 0; i < 7; i++) {
-                    System.out.print("Enter expenses for " + daysOfWeek[i] + ": ");
-                    expenses[i] = sc.nextDouble();
-                }
-
-                // Calculate total weekly expense
-                double totalExpense = 0;
-                for (double expense : expenses) {
-                    totalExpense += expense;
-                }
-
-                // Print total weekly expense
-                System.out.printf("Total weekly expense week ", weeknum, " : $%.2f\n", totalExpense);
-                budget.ChkExceedBudget(totalExpense, weeknum);
             }
         }
     }
-}
-
-
