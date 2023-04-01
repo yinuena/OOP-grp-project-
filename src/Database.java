d9abda4e54861def2e574c29fcd51239adf4e965
yinuena <126445864+yinuena@users.noreply.github.com>
2023-04-02 01:25:19 +0800

    Update Database.java

--- src/Database.java
+++ src/Database.java
@@ -10,6 +10,7 @@ public class Database {
 
     private static String driver = "com.mysql.cj.jdbc.Driver"; //static means its a class level variable
 
+
     public Database(String url, String port, String db,
                     String username, String passwd) {
         try {
@@ -31,7 +32,7 @@ public class Database {
 
     }
 
-    public void insert(String username, String email, String password, int age, String fname, String mname, String lname) {
+    public void insertRegistration(String username, String email, String password, int age, String fname, String mname, String lname) {
         try {
             Statement stmt = con.createStatement();
             stmt.executeUpdate("INSERT INTO `registration`(username, email,password,age,fname, mname, lname) VALUE ('"+username+"','"+email+"','"+password+"',"+age+",'"+fname+","+mname+","+lname+"')");
@@ -39,6 +40,22 @@ public class Database {
         }
         catch(Exception e){ System.out.println(e);}
 
+    }
+
+    //insert into Budget database table
+    public void insertBudget( String date, float daybudget, int week , float budget){
+        try{
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate("INSERT INTO 'Budget'(date, week, budget) VALUE( '"+date+"', '"+daybudget+"','"+week+"','"+budget+"')");
+        }catch(Exception e){System.out.println(e); }
+    }
+
+    //insert into Expense database table
+    public void insertExpense( String date, double dayexpense, int week , double expense){
+        try{
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate("INSERT INTO 'Expense'(date, week, expense) VALUE( '"+date+"','"+dayexpense+"', '"+week+"','"+expense+"')");
+        }catch(Exception e){System.out.println(e); }
     }
 
     public void close() {
